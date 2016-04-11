113246b0708301956f3c394660a8fe099fcc592b
Allan Taschini <allan.taschini@gmail.com>
2016-04-11 11:00:33 +0200

    Add section 2 related code.
    From the "Pure Functions. f(d)=v. Props and Nesting Components" section

--- app/index.js
+++ app/index.js
@@ -1,15 +1,53 @@
+var USER_DATA = {
+  name: 'Allan Taschini',
+  username: 'atask',
+  image: 'http://www.gravatar.com/avatar/43134c1164e5e9644314bae984e34cb5'
+}
+
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function () {
     return (
-      <div> Hello ReactJS Program </div>
+      <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+    )
+  }
+})
+
+var ProfileLink = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+})
+
+var ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+})
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 })
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 )
